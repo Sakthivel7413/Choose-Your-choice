9ede85277f966246de3420485f35165a8312d2ac
sakthive M <109778366+Sakthivel7413@users.noreply.github.com>
2024-09-27 15:43:16 +0530

    Update GamePage.cs

--- Assets/Scripts/GamePage.cs
+++ Assets/Scripts/GamePage.cs
@@ -1,11 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GamePage : MonoBehaviour
+public class GamePage : MonoBehaviour, IObserver
 {
     [SerializeField] private Button YesButton, NoButton;
+    [SerializeField] private TMP_Text questionText;
+    private LevelManager levelManager;
 
+    private void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+        if (levelManager != null)
+        {
+            levelManager.RegisterObserver(this); 
+        }
+    }
 
+    private void OnYesButtonClicked()
+    {
+        levelManager.CheckAnswer(true);
+    }
+
+    private void OnNoButtonClicked()
+    {
+        levelManager.CheckAnswer(false);
+    }
+
+    private void OnDestroy()
+    {
+        if (levelManager != null)
+        {
+            levelManager.UnregisterObserver(this); 
+        }
+    }
+
+    public void OnNotify(string message)
+    {
+        Debug.Log(message);
+        questionText.text = message;  
+    }
 }
